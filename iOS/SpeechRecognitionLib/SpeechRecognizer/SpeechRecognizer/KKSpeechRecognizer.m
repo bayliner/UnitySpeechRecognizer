daa27eb361a70a8e53228658362621d347fbff5d
bay <bayliner78@gmail.com>
2022-03-07 11:01:08 +0900

    bug fix try

--- iOS/SpeechRecognitionLib/SpeechRecognizer/SpeechRecognizer/KKSpeechRecognizer.m
+++ iOS/SpeechRecognitionLib/SpeechRecognizer/SpeechRecognizer/KKSpeechRecognizer.m
@@ -89,96 +89,123 @@ KKSpeechRecognitionAuthorizationStatus KKSpeechRecognitionAuthorizationStatusFro
 }
 
 - (void)startRecording:(RecognitionOptions)options {
-    if (_recognitionTask != nil) {
-        [_recognitionTask cancel];
-        _recognitionTask = nil;
-    }
     
-    NSError *error;
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    _defaultAudioSessionCategory = audioSession.category;
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
-    [audioSession setMode:AVAudioSessionModeDefault error:&error];
-    [audioSession setActive:YES error:&error];
+    dispatch_async(dispatch_get_main_queue(), ^{
     
-    if (error != nil) {
-        NSLog(@"KKSpeechRecognizer: AVAudioSession couldn't be created");
-        [self sendStartRecordingErrorMessage:[NSString stringWithFormat:@"%@", error.userInfo]];
-        return;
-    }
-    
-    AVAudioInputNode *inputNode = _audioEngine.inputNode;
-    if (inputNode == nil) {
-        [self sendStartRecordingErrorMessage:@"AVAudioInputNode couldn't be created"];
-        return;
-    }
-    
-    _recognitionRequest = [SFSpeechAudioBufferRecognitionRequest new];
-    if (_recognitionRequest == nil) {
-        [self sendStartRecordingErrorMessage:@"AudioBufferRecognitionRequest couldn't be created"];
-        return;
-    }
-    
-    _recognitionRequest.shouldReportPartialResults = options.shouldCollectPartialResults;
-    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"13.0")) {
-        _recognitionRequest.requiresOnDeviceRecognition = options.requiresOnDeviceRecognition;
-    }
-    
-    _recognitionTask = [_internalRecognizer recognitionTaskWithRequest:_recognitionRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
-        BOOL isFinal = NO;
-        if (result != nil) {
-            isFinal = result.isFinal;
-            if (result.isFinal) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [_delegate speechRecognizer:self gotFinalResult:result.bestTranscription.formattedString];
-                });
-            } else {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [_delegate speechRecognizer:self gotPartialResult:result.bestTranscription.formattedString];
-                });
-            }
+        // https://stackoverflow.com/questions/59238035/avaudioengine-causes-augraphparserinitializeactivenodesininputchain-error-on-i
+        //_audioEngine = [AVAudioEngine new];
+        
+        if (_recognitionTask != nil) {
+            [_recognitionTask cancel];
+            _recognitionTask = nil;
         }
         
-        if (error != nil || isFinal) {
-            if (error != nil) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [_delegate speechRecognizer:self failedDuringRecordingWithReason:[NSString stringWithFormat:@"%@", error]];
-                });
-            }
-//
-            [_audioEngine stop];
-            [inputNode removeTapOnBus:0];
-            
-            _recognitionRequest = nil;
-            _recognitionTask = nil;
-            // [self stopRecording];
+        NSError *error;
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        _defaultAudioSessionCategory = audioSession.category;
+        //try audioSession.setCategory(.playAndRecord, mode: .spokenAudio, options: .defaultToSpeaker)
+        //try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+        [audioSession setMode:AVAudioSessionModeDefault error:&error];
+        [audioSession setActive:YES error:&error];
+        //[audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:&error];
+        
+        //[NSThread sleepForTimeInterval:0.1];    // audioSession setActive need some time
+        
+        
+        if (error != nil) {
+            NSLog(@"KKSpeechRecognizer: AVAudioSession couldn't be created");
+            [self sendStartRecordingErrorMessage:[NSString stringWithFormat:@"%@", error.userInfo]];
+            return;
         }
-    }];
-    
-    AVAudioFormat *format = [inputNode outputFormatForBus:0];
-    
-    @try {
-        [inputNode installTapOnBus:0 bufferSize:1024 format:format block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
-            [_recognitionRequest appendAudioPCMBuffer:buffer];
+        
+        AVAudioInputNode *inputNode = _audioEngine.inputNode;
+        if (inputNode == nil) {
+            [self sendStartRecordingErrorMessage:@"AVAudioInputNode couldn't be created"];
+            return;
+        }
+        
+        _recognitionRequest = [SFSpeechAudioBufferRecognitionRequest new];
+        if (_recognitionRequest == nil) {
+            [self sendStartRecordingErrorMessage:@"AudioBufferRecognitionRequest couldn't be created"];
+            return;
+        }
+        
+        _recognitionRequest.shouldReportPartialResults = options.shouldCollectPartialResults;
+        if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"13.0")) {
+            _recognitionRequest.requiresOnDeviceRecognition = options.requiresOnDeviceRecognition;
+        }
+        
+        _recognitionTask = [_internalRecognizer recognitionTaskWithRequest:_recognitionRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
+            
+            //dispatch_async(dispatch_get_main_queue(), ^{
+                
+                BOOL isFinal = NO;
+                if (result != nil) {
+                    isFinal = result.isFinal;
+                    if (result.isFinal) {
+                        //dispatch_async(dispatch_get_main_queue(), ^{
+                            [_delegate speechRecognizer:self gotFinalResult:result.bestTranscription.formattedString];
+                        //});
+                    } else {
+                        //dispatch_async(dispatch_get_main_queue(), ^{
+                            [_delegate speechRecognizer:self gotPartialResult:result.bestTranscription.formattedString];
+                        //});
+                    }
+                }
+                
+                if (error != nil || isFinal) {
+                    if (error != nil) {
+                        //dispatch_async(dispatch_get_main_queue(), ^{
+                            [_delegate speechRecognizer:self failedDuringRecordingWithReason:[NSString stringWithFormat:@"%@", error]];
+                        //});
+                    }
+
+                    [_audioEngine stop];
+                    [inputNode removeTapOnBus:0];
+                    
+                    _recognitionRequest = nil;
+                    _recognitionTask = nil;
+                    // [self stopRecording];
+                }
+                
+            //});
+            
         }];
-    }
-    @catch (NSException *exception) {
-        NSLog(@"%@", exception.userInfo);
-        [self sendStartRecordingErrorMessage:[NSString stringWithFormat:@"%@", exception.userInfo]];
-        return;
-    }
-    
-    [_audioEngine prepare];
-    
-    NSError *startError;
-    [_audioEngine startAndReturnError:&startError];
-    
-    if (startError != nil) {
-        [self sendStartRecordingErrorMessage:[NSString stringWithFormat:@"%@", startError.userInfo]];
-    }
+        
+        AVAudioFormat *format = [inputNode outputFormatForBus:0];
+        
+        @try {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                [inputNode installTapOnBus:0 bufferSize:1024 format:format block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
+                    [_recognitionRequest appendAudioPCMBuffer:buffer];
+                }];
+                
+            }); // dispatch_async(dispatch_get_main_queue(), ^{
+        }
+        @catch (NSException *exception) {
+            NSLog(@"exception.userInfo - %@", exception.userInfo);
+            [self sendStartRecordingErrorMessage:[NSString stringWithFormat:@"%@", exception.userInfo]];
+            return;
+        }
+        
+        [_audioEngine prepare];
+        
+        NSError *startError;
+        [_audioEngine startAndReturnError:&startError];
+
+        if (startError != nil) {
+            NSLog(@"startError - %@", startError.userInfo);
+            [self sendStartRecordingErrorMessage:[NSString stringWithFormat:@"%@", startError.userInfo]];
+        }
+        
+    }); // dispatch_async(dispatch_get_main_queue(), ^{
 }
 
 - (void)stopRecording {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
     [_audioEngine stop];
 //    [_audioEngine.inputNode removeTapOnBus:0];
 
@@ -190,6 +217,8 @@ KKSpeechRecognitionAuthorizationStatus KKSpeechRecognitionAuthorizationStatusFro
     if (_defaultAudioSessionCategory) {
         [audioSession setCategory:_defaultAudioSessionCategory error:nil];
     }
+        
+    }); // dispatch_async(dispatch_get_main_queue(), ^{
 }
 
 - (void)stopIfRecording {
